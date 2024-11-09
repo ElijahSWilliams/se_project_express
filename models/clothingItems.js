6da0b92f9c7e3e85086411dcd4b9d80b71d8413f
Elijah <xrealntrue16x@gmail.com>
2024-11-08 20:29:13 -0500

    enum weather values creadted

--- models/clothingItems.js
+++ models/clothingItems.js
@@ -1,5 +1,27 @@
 const mongoose = require("mongoose");
 
-const clothingItemSchema = new mongoose.Schema({});
+const WEATHER = {
+  cold: "cold",
+  warm: "warm",
+  hot: "hot",
+};
+
+const clothingItemSchema = new mongoose.Schema({
+  name: {
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  weather: {
+    required: true,
+    minlength: 2,
+    maxlength: 4,
+    enum: [WEATHER.cold, WEATHER.warm, WEATHER.hot],
+  },
+  imageUrl: {},
+  owner: {},
+  likes: {},
+  createdAt: {},
+});
 
 module.exports = mongoose.model("item", clothingItemSchema);
