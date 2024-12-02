cede6b145fb7c750a97313ef2ae486ea6b8451cc
Elijah <xrealntrue16x@gmail.com>
2024-12-02 17:53:17 -0500

    User Schema Email and Password Updated

--- models/user.js
+++ models/user.js
@@ -13,11 +13,28 @@ const userSchema = new mongoose.Schema({
     required: [true, "Avatar Link Required."],
     validate: {
       validator(value) {
-        return validator.isURL(value);
+        return validator.isURL(value); // Ensures the avatar is a valid URL
       },
       message: "You must enter a valid URL",
     },
   },
+  email: {
+    type: String,
+    required: [true, "Email Required."],
+    validate: {
+      validator(value) {
+        return validator.isEmail(value); // Ensures the email is valid
+      },
+      message: "You must enter a valid email",
+    },
+    unique: true, // Ensures the email is unique across users
+  },
+  password: {
+    type: String,
+    required: [true, "Enter a password"],
+  },
 });
 
-module.exports = mongoose.model("user", userSchema);
+// Export the model
+const User = mongoose.model("User", userSchema);
+module.exports = User;
