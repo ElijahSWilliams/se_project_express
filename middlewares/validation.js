a33bd19a55fb57435cf9d4c61c8143f7d6747507
Elijah <xrealntrue16x@gmail.com>
2025-02-18 19:21:27 -0500

    Middlewares

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -50,14 +50,16 @@ const validateUser = celebrate({
 });
 
 const validateUserUpdates = celebrate({
-  name: Joi.string().required().min(2).max(30).messages({
-    "string.min": "The minimun length of the 'name' field is 2",
-    "string.max": "The maximum length of the 'name' field is 30",
-    "string.empty": 'The "name" field must be filled in',
-  }),
-  avatar: Joi.string().required().custom(validateURL).messages({
-    "string.empty": 'The "imageUrl" field must be filled out',
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30).messages({
+      "string.min": "The minimun length of the 'name' field is 2",
+      "string.max": "The maximum length of the 'name' field is 30",
+      "string.empty": 'The "name" field must be filled in',
+    }),
+    avatar: Joi.string().required().custom(validateURL).messages({
+      "string.empty": 'The "imageUrl" field must be filled out',
       "string.uri": 'the "imageUrl" field must be a valid url ',
+    }),
   }),
 });
 
@@ -81,5 +83,5 @@ module.exports = {
   validateUser,
   validateLogin,
   validateID,
-  validateUserUpdates
+  validateUserUpdates,
 };
